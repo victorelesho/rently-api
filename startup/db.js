426b3f5988bb2ddca8b5c1b36d503d465cf0f8ff
Elesho Victor <victoreleshosegun@gmail.com>
2022-06-30 11:39:21 +0100

    Upgraded mongoose.

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,7 @@ const config = require('config');
 
 module.exports = function () {
     const db = config.get('db');
+    winston.info(`${db}`);
     mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
